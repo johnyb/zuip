9519b3b9fe7da526e891d78bb2a6397c4cf61182
Julian Bäume <julian@svg4all.de>
2011-05-27 11:49:37 +0200

    add first rspecs

---
+++ spec/presentations/presentation_spec.rb
@@ -0,0 +1,14 @@
+describe "presentations" do
+
+  it "can be instantiated" do
+    p = Presentation.new
+  end
+
+  it "should have a title" do
+    p = Presentation.new
+    p.title = "test title"
+    p.title.should == "test title"
+  end
+
+end
+
