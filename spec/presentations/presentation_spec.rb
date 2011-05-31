388b7cfd2aac941d9c0f53133d1179e3e740ddb3
Julian Bäume <julian@svg4all.de>
2011-05-31 18:30:51 +0200

    make rpsec tests run

--- spec/presentations/presentation_spec.rb
+++ spec/presentations/presentation_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe "presentations" do
 
   it "can be instantiated" do
-    p = Presentation.new
+    p = ZUIP::Presentation.new
   end
 
   it "should have a title" do
-    p = Presentation.new
+    p = ZUIP::Presentation.new
     p.title = "test title"
     p.title.should == "test title"
   end
