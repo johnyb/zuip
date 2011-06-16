5eafe9b616e7e7b95d89f43c7dd60e686303ae0f
Julian Bäume <julian@svg4all.de>
2011-06-16 13:33:49 +0200

    implement outline regarding spec
    it passes, now

--- lib/zuip/presentation.rb
+++ lib/zuip/presentation.rb
@@ -1,6 +1,12 @@
 module ZUIP
   class Presentation
     attr_accessor :title
+    attr_reader :outline
+
+    def outline=(o)
+      raise ArgumentException unless o.class == Array
+      @outline = o
+    end
   end
 end
 
