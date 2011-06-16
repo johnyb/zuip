aec3a353e4e0fea87a89e422c89d3b347ecf8c46
Julian Bäume <julian@svg4all.de>
2011-06-16 13:31:26 +0200

    make the project a rails app
    * update cucumber and rspec helpers for use in rails
    * initailize rails app
    * install jquery for rails
        remove prototype. soon there will be rails 3.1 with jquery as
        default and I guess it’s better to switch early (before
        development of own JS-code started)

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~> 3.0'
+gem 'jquery-rails'
+
+gem 'sqlite3'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+group :development, :test do
+  gem 'webrat'
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zuip::Application.config.secret_token = 'ee562682e69cbfaf7676d44aff3e48153c7f3c27ff366aeccb5ed42dd62ac794ad98b79e175dd9ee3ab8e5ef882e385ecfbf79729b93fab3d3970b96f93149dc'
