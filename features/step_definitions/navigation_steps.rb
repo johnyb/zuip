a0ec10e5c14a511c03b17c46635445597fd05089
Julian Bäume <julian@svg4all.de>
2011-06-19 23:32:24 +0200

    add step definition stubs for navigation features
    these are gonna be used in iteration 1

---
+++ features/step_definitions/navigation_steps.rb
@@ -0,0 +1,31 @@
+Given /^the current position is at "([^"]*)"$/ do |name|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I can see an element "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I zoom in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the element "([^"]*)" should be larger$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I zoom out$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the element "([^"]*)" should be smaller$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I pan (\d+) pixels to the left$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the element "([^"]*)" should be moved by \(\-(\d+),(\d+)\)$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
