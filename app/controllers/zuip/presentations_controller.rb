0646e5dc41a688744ecaf9a9b4df66378d9da3b7
Julian Bäume <julian@svg4all.de>
2011-06-22 23:23:02 +0200

    fix Zuip::PresentationController
    was broken by refactoring steps

--- app/controllers/zuip/presentations_controller.rb
+++ app/controllers/zuip/presentations_controller.rb
@@ -1,7 +1,10 @@
 class Zuip::PresentationsController < ApplicationController
   layout 'zuip'
+  include Zuip::PresentationsHelper
+
   def show
-    presentation = Zuip::Presentation.new
+    path = File.join([Rails.root.to_s, "public", zuip_path(params[:name])])
+    presentation = Zuip::Presentation.new(:source => path)
     render :locals => { :presentation => presentation, :name => params[:name] }
   end
 
