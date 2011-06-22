8e2fa67bcd6cd6ab8e410527aa36e12bfaf6ffd8
Julian Bäume <julian@svg4all.de>
2011-06-22 23:23:27 +0200

    implement Zuip::Presentation spec
    all tests that were green, are green again

--- lib/zuip/presentation.rb
+++ lib/zuip/presentation.rb
@@ -1,21 +1,31 @@
+require 'nokogiri'
+
 module Zuip
   class Presentation
-    attr_accessor :title, :path
-    attr_reader :outline, :file
+    attr_reader :outline
+
+    def initialize(s)
+      self.source=s[:source]
+    end
+
+    def title
+      @doc.css('title').first.content
+    end
 
     def outline=(o)
       raise ArgumentException unless o.class == Array
       @outline = o
     end
 
-    def path=(p)
-      @path = p
+    def source=(s)
+      @source = s
       reload
     end
 
     private
     def reload
-      @file = File.new(path)
+      raise Errno::ENOENT unless File.exists?(@source)
+      @doc = Nokogiri::XML.parse( File.new(@source) )
     end
   end
 end
