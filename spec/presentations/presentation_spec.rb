8906fb5368cdbd598ce17eb1e010343232396be7
Julian Bäume <julian@svg4all.de>
2011-07-04 09:00:10 +0200

    fix indentation

--- spec/presentations/presentation_spec.rb
+++ spec/presentations/presentation_spec.rb
@@ -9,40 +9,40 @@ describe "A Presentation" do
 
   context "with information parsed from the SVG file:" do
     describe "the outline" do
-    let(:outline) { ["# Title #",
-                     "# Slides #",
-                     "## Advantages ##",
-                     "## Problems ##",
-                     "# ZUI Presentations #",
-                     "## Advantages ##",
-                     "## Problems ##",
-                     "# Solutions #",
-                     "## Prezi ##",
-                     "## CounterPoint ##",
-                     "## ZUIP ##"]
-                  }
+      let(:outline) { ["# Title #",
+                      "# Slides #",
+                      "## Advantages ##",
+                      "## Problems ##",
+                      "# ZUI Presentations #",
+                      "## Advantages ##",
+                      "## Problems ##",
+                      "# Solutions #",
+                      "## Prezi ##",
+                      "## CounterPoint ##",
+                      "## ZUIP ##"]
+                    }
 
-    it "should have a title" do
-      p.title.should == "About Zooming Presentations"
-    end
-
-    it "should raise an error if no title is found" do
-      xml = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-      <svg xmlns:dc="http://purl.org/dc/elements/1.1/">
-      <metadata><rdf:RDF>
-      </rdf:RDF></metadata>
-      </svg>'
-      p.instance_eval {@doc = Nokogiri::XML.parse(xml) }
-      begin
-        p.title.should be_nil
-      rescue Zuip::ParsingError, Nokogiri::XML::XPath::SyntaxError
-        #these are fine
+      it "should have a title" do
+        p.title.should == "About Zooming Presentations"
       end
-    end
 
-    it "should have an outline provided as an array" do
-      p.outline.should == outline
-    end
+      it "should raise an error if no title is found" do
+        xml = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        <svg xmlns:dc="http://purl.org/dc/elements/1.1/">
+        <metadata><rdf:RDF>
+        </rdf:RDF></metadata>
+        </svg>'
+        p.instance_eval {@doc = Nokogiri::XML.parse(xml) }
+        begin
+          p.title.should be_nil
+        rescue Zuip::ParsingError, Nokogiri::XML::XPath::SyntaxError
+          #these are fine
+        end
+      end
+
+      it "should have an outline provided as an array" do
+        p.outline.should == outline
+      end
     end
   end
 end
