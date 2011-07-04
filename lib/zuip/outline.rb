3580d1650be16703d0b7ef4e586bdc4b5e9f577b
Julian Bäume <julian@svg4all.de>
2011-07-04 19:30:11 +0200

    implement waypoint info for bounding box
    test passes

--- lib/zuip/outline.rb
+++ lib/zuip/outline.rb
@@ -8,8 +8,7 @@ module Zuip
     def find_waypoints(root, level = "#", headlines = [])
       waypoints = root>("g")
       waypoints.each do |wp|
-        title = (wp > "title").first.content
-        headlines << {:name => wrap(title, level)}
+        headlines << wp_info(wp, level)
         find_waypoints wp, level+"#", headlines
       end
       headlines
@@ -17,6 +16,18 @@ module Zuip
 
     def wrap(s, w)
       "#{w} #{s} #{w}"
+    end
+
+    def wp_info(wp, level)
+      title = (wp > "title").first.content
+      rect = (wp > "use").first
+      translate, scale = rect.attribute("transform").value.split(" ")
+      translate.match /^translate\((.*),(.*)\)$/
+      x = $1.to_f
+      y = $2.to_f
+      scale.match /^scale\((.*)\)$/
+      scale = $1.to_f
+      {:name => wrap(title, level), :x => x, :y => y, :scale => scale}
     end
   end
 end
