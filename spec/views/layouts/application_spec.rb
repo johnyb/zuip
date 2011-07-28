1295cbe04683cbe3ed27edc2d83408628194de0c
Julian Bäume <julian@svg4all.de>
2011-07-28 15:45:19 +0200

    add some additional information to app layout
    * show ZUIP in the title
    * show an error flash message if set by the controller

--- spec/views/layouts/application_spec.rb
+++ spec/views/layouts/application_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe "layouts/application" do
+  it "should render error notices" do
+    flash[:error] = "Error"
+    render
+    rendered.should have_css("div#error", :text => "Error")
+  end
+
   it "should render alert notices" do
     flash[:alert] = 'Error'
     render
-    rendered.should have_css("div#alert", :content => 'Error')
+    rendered.should have_css("div#alert", :text => 'Error')
   end
 
   it "should not render the alert div without flash" do
