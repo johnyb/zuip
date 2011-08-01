78eba6e18b3fea6469b1eef4b925db3891dfc618
Julian Bäume <julian@svg4all.de>
2011-08-01 10:25:41 +0200

    add edit link for presentations to index
    since soon there will be an editor!

--- spec/views/zuip/presentations/index.html.erb_spec.rb
+++ spec/views/zuip/presentations/index.html.erb_spec.rb
@@ -1,14 +1,29 @@
 require 'spec_helper'
 
 describe 'zuip/presentations/index.html.erb' do
-  it "should list presentations" do
-    p = []
-    p << double(Zuip::Presentation, :title => "Testpresentation", :fileName => "test.svg")
-    p << double(Zuip::Presentation, :title => "Another Testpresentation", :fileName => "test2.svg")
-    assign(:presentations, p)
-    render
-    rendered.should have_css("ol > li", :text => "Testpresentation")
-    rendered.should have_css("ol > li", :text => "Another Testpresentation")
+  describe "the list of presentations" do
+    before(:each) do
+      p = []
+      p << double(Zuip::Presentation, :title => "Testpresentation", :fileName => "test.svg")
+      p << double(Zuip::Presentation, :title => "Another Testpresentation", :fileName => "test2.svg")
+      assign(:presentations, p)
+      render
+    end
+
+    it "should render some presentations" do
+      rendered.should have_css("ol > li", :text => "Testpresentation")
+      rendered.should have_css("ol > li", :text => "Another Testpresentation")
+    end
+
+    it "should have links to show each presentation" do
+      rendered.should have_link("Testpresentation", {:href => named_presentations_path("test")})
+      rendered.should have_link("Another Testpresentation", {:href => named_presentations_path("test2")})
+    end
+
+    it "should have links to edit each presentation" do
+      rendered.should have_link("Edit", {:href => edit_presentations_path("test")})
+      rendered.should have_link("Edit", {:href => edit_presentations_path("test2")})
+    end
   end
 
   it "should provide a link to the create page" do
